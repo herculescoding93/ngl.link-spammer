8b9dfbcc4f352ba05b40a686000eb5bb736eca23
herculescoding93 <iraklis.ventouras@gmail.com>
2023-06-06 00:23:29 +0300

    Migrated some files

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 # ngl.link-spammer
 Spammer for the popular website ngl.link
-I do not promote using this tool for harrasment or bullying, i coded this to prove that such automation is possible with python
+Normally, ngl.link throttles a user after 25 messages, but with threads, we can simply send lets say, 250 messages at the same time so it wouldnt throttle us
+To enable proxies, set "proxies" to true in "config.json"
+If you enable proxies, each message has a random proxy assigned to it
+If you want to enable random messages, set "randommsgs" to true in "config.json"
+If you want to add random messages, do so in msg.py
+To run the code, you must have python and pip installed on your system
+If you do have those installed, run "pip install -r requirements.txt"
+When pip finishes, run "python main.py" and fill out the form and you are done!
 
+
+THIS IS AGAINST NGL.LINK TOS AND I AM NOT RESPONSIBLE FOR ANY DAMAGE CAUSED FROM USING THIS TOOL
+I do not promote using this tool for harrasment or bullying, i coded this to prove that such automation is possible with python
 This software is licensed under MIT license, check the "LICENSE" file for more information if your planning on modifying/distributing this tool
---
+++ main.py
@@ -0,0 +1,76 @@
+#Libraries
+import httpx
+import random
+from colorama import Fore
+import threading
+from msg import randomQuestions
+import json
+
+#Headers required to make ngl think that we are a real user and not a bot
+headers = {
+    "Content-Type": "application/x-www-form-urlencoded",
+    "Accept-Language": "en-US,en;q=0.5",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+    "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:104.0) Gecko/20100101 Firefox/104.0",
+}
+
+#function to generate random 36 character length device ids 
+def deviceid():
+    return "".join(
+        random.choice("0123456789abcdefghijklmnopqrstuvwxyz-") for i in range(36)
+    )
+
+#function for selecting a random proxy every message
+def proxy():
+    return "http://" + random.choice(list(open("proxies.txt")))
+
+f = []
+
+#main spam function
+def spam(target, msg):
+    if messagestatus:
+        msg = randomQuestions[random.randint(0, len(randomQuestions) - 1)]
+    if proxystatus:
+        proxy1 = {"http://": proxy().rstrip()}
+        client = httpx.Client(headers=headers, proxies=proxy1)
+    else:
+        client = httpx.Client(headers=headers)
+    
+    postresp = client.post(
+        f"https://ngl.link/api/submit",
+        data={
+            "username": target,
+            "question": msg,
+            "deviceId": deviceid,
+        } 
+    
+    )
+    f.append(True)
+    return postresp.status_code
+
+#main code
+threads = []
+def handler(totalMsgs, target, message):
+    for i in range(totalMsgs):
+        t = threading.Thread(target=spam, kwargs={"target": target, "msg": message})
+        threads.append(t)
+    for i in range(totalMsgs):
+        threads[i].start()
+    for i in range(totalMsgs):
+        threads[i].join()
+    while len(f) < totalMsgs:
+        continue
+    print(f"{Fore.RED}Messages got {Fore.WHITE}sent!")
+
+with open("config.json") as config:
+    data = json.load(config)
+    proxystatus = data["proxies"]
+    messagestatus = data["randommsgs"]
+username = input(f"{Fore.GREEN}Enter full {Fore.BLUE}username(with numbers)\n")
+message = "1"
+if not messagestatus:
+    message = input(f"{Fore.CYAN}Enter the message you want to send\n")
+
+messages = int(input(f"{Fore.RED}Enter the amount of messages you want to be sent to the user\n"))
+handler(messages, username, message)
+
